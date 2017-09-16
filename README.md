96a90e8622fa4edbe7ea3ced0a6ed3b1e0373934
Bremen Braun <bremen.braun@gmail.com>
2017-09-15 20:10:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# All Hallows' Eve Plus
+A color theme for VS Code based on the colors of Halloween
---
+++ test.js
@@ -0,0 +1,25 @@
+import test from 'test'
+
+class TestClass extends Thing {
+    constructor () {
+        let str = 'string'
+        let number = 90
+        let obj = {
+            key: number
+        }
+    }
+}
+
+function test (games) {
+    const game = games.get(args.id)
+}
+
+// line comment
+/**
+ * 
+ * Block comment
+ * 
+ */
+const fn = (param1, param2, param3) => {
+    param1.map(val => val * 12)
+}
